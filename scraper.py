dc8b4287b60a555ad04e7e45357d7a8701624116
Jayesh Prajapati <jayeshprajapati.work@gmail.com>
2026-08-16 00:16:20 +0530

    Update scraper to fetch and display job data
    Fetch job listings from Greenhouse API and display total count.

--- scraper.py
+++ scraper.py
@@ -1,1 +1,14 @@
-print("Hello from Python")
+import requests
+
+print("GitHub Actions is running Python successfully")
+
+response = requests.get(
+    "https://boards-api.greenhouse.io/v1/boards/notion/jobs",
+    timeout=30
+)
+
+print("Status Code:", response.status_code)
+
+data = response.json()
+
+print("Total Jobs:", len(data["jobs"]))
