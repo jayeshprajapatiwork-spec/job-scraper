dcbb5315ce4367da91f636ebab51d4ee3c338672
Jayesh Prajapati <jayeshprajapati.work@gmail.com>
2026-08-16 00:23:07 +0530

    Change job board URL and process JSON response
    Updated the job board URL and added JSON response handling.

--- scraper.py
+++ scraper.py
@@ -1,8 +1,12 @@
 import requests
 
-url = "https://boards-api.greenhouse.io/v1/boards/notion/jobs"
+url = "https://boards-api.greenhouse.io/v1/boards/cloudflare/jobs"
 
 response = requests.get(url, timeout=30)
 
 print("Status Code:", response.status_code)
-print(response.text[:500])
+
+data = response.json()
+
+print("Keys:", data.keys())
+print("Total Jobs:", len(data["jobs"]))
